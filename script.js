91c775a1071e3b3ea893869955aa6755a19f0562
Nehabobade2001 <118434258+Nehabobade2001@users.noreply.github.com>
2023-04-21 05:55:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var tl=gsap.timeline()
+tl.from("#nav",{
+    y:-20,
+    opacity:0,
+    duration:0.6,
+    delay:0.2
+})
+.from("#left h2",{
+    x:-20,
+    opacity:0,
+    duration:0.6,
+    delay:0.2
+})
+.from("#left h3",{
+    x:-20,
+    opacity:0,
+    duration:0.6,
+    delay:0.2
+})
+.from("#left h5",{
+    x:-20,
+    opacity:0,
+    duration:0.6,
+    delay:0.2
+})
+.from("#left p",{
+    x:-20,
+    opacity:0,
+    duration:0.6,
+    delay:0.2
+})
+.from("#left button",{
+    x:-20,
+    opacity:0,
+    duration:0.6,
+    delay:0.2
+})
+.from("#main>img",{
+    x:-20,
+    opacity:0,
+    duration:0.6,
+    delay:0.2,
+    rotate:210
+})
